097450719ad6db13a1b00f946cdefc3ab278b875
marmooo <marmooo@users.noreply.github.com>
2025-11-17 00:19:12 +0900

    improve UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { toPixelData } from "https://cdn.jsdelivr.net/npm/html-to-image@1.11.11/+esm";
 import { createWorker } from "https://cdn.jsdelivr.net/npm/emoji-particle@0.0.4/+esm";
 
-document.getElementById("gamePanel")
-  .appendChild(document.getElementById("gamePanelTemplate").content);
 const courseNode = document.getElementById("course");
 const emojiParticle = initEmojiParticle();
 let audioContext;
@@ -503,9 +501,8 @@ async function nextProblem() {
   removeUseTags(svg);
   uniqIds(svg);
 
-  const targets = document.querySelectorAll("#problems .iconContainer");
-  targets[0].replaceChildren(tehon);
-  targets[1].replaceChildren(svg);
+  document.getElementById("tehon").replaceChildren(tehon);
+  document.getElementById("pieces").replaceChildren(svg);
 
   resetTransforms(svg);
   initPieces(svg);
